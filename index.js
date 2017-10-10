fc19874cbe0c5d0432e03fdd68242aeb5177acc2
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-10 21:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
+<<<<<<< HEAD
   const rankedLists = document.querySelectorAll(".ranked-list")
   const firstList = rankedLists[0]
   const secondList = rankedLists[1]
@@ -19,14 +20,33 @@ function increaseRankBy(n){
    for (let i = 0, l = children.length; i < l; i++){
     var val = parseInt(children[i].innerHTML);
      children[i].innerHTML = val + n
+=======
+  const rankedList = document.querySelectorAll("ul.ranked-list li")
+  const firstList = rankedList[0]
+  const secondList = rankedList[1]
+
+  let children = firstList.children;
+   for (let i = 0; i < children.length; i++){
+     var val = children[i].innerHTML;
+     return children[i].innerHTML = parseInt(val + n)
+>>>>>>> 14023313ef99517b13818fe1ee5bb1c87ce97541
    }
 
    children = secondList.children;
     for (let i = 0, l = children.length; i < l; i++){
+<<<<<<< HEAD
       var val = parseInt(children[i].innerHTML);
        children[i].innerHTML = val + n
     }
    /* */
+=======
+      var val = children[i].innerHTML;
+      return children[i].innerHTML = parseInt(val - n)
+    }
+
+   /* */
+   return
+>>>>>>> 14023313ef99517b13818fe1ee5bb1c87ce97541
 }
 
 function deepestChild(){
